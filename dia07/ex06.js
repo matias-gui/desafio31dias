fb39667f45755cecdb5e8c21550135fea47e3896
Guilherme Matias <guimatias890@gmail.com>
2025-01-23 18:37:33 -0300

    mais um exercício de função

---
+++ dia07/ex06.js
@@ -0,0 +1,7 @@
+// Essa função irá receber uma string e retornará quantos caracteres tem 
+
+function quantoscaracteres(){
+    const nome = "Paulo Guilherme"
+    return nome.length
+}
+console.log(quantoscaracteres())
