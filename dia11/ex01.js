173ab834be50f3969734b58a0d9c87f322e84463
Guilherme Matias <guimatias890@gmail.com>
2025-01-29 21:00:23 -0300

    exercício encapsulamento

--- dia11/ex01.js
+++ dia11/ex01.js
@@ -1,4 +1,4 @@
-
+// Classe com saldo privado e métodos para depositar, sacar e consultar saldo
 
 class ContaBancaria{
     #saldo 
@@ -7,18 +7,32 @@ class ContaBancaria{
         this.#saldo = saldo
     }
 
-    depositar(depositar){
-        this.#saldo += depositar 
+    depositar(valorDepositar){
+        if(valorDepositar <= 0 ){
+            console.log("Valor inválido !!")
+            return
+        }
+        this.#saldo += valorDepositar 
+        console.log("Valor", valorDepositar,"depositado")
     }
-    sacar(sacar){
-        if(sacar > this.#saldo){
+    sacar(valorSacar){
+        if(valorSacar <= 0 ){
+            console.log("Valor inválido !!")
+        }
+        if(valorSacar > this.#saldo){
             console.log("Saldo insuficiente")
         }else{
-            this.#saldo -= sacar
-            console.log("Saque feito")
+            this.#saldo -= valorSacar
+            console.log(`Saque de ${valorSacar} realizado com sucesso!!`)
         }
     }
-    consultarsaldo(novosaldo){
+    consultarSaldo(){
+        console.log("O saldo atual é", this.#saldo)
         
     }
 }
+
+const caixa = new ContaBancaria(1000)
+caixa.depositar(3000)
+caixa.sacar(20)
+caixa.consultarSaldo()
